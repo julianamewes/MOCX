dccd0eadb133fc5105b9a1a924f3130e95df8cc4
julianamewes <95439636+julianamewes@users.noreply.github.com>
2021-12-06 21:06:22 -0300

    teste2 6-12

--- cadastro_cpf.php
+++ cadastro_cpf.php
@@ -1,17 +1,14 @@
 <?php
    include_once("conexao.php"); 
 
-     $nome=$_POST['nome'];
-     $data_nascimento= date('d-m-y');
-     $cpf=$_POST['cpf'];
+     $nome= $_POST["nome"];
+     $data_nascimento= $_POST["data_nascimento"];
+     $cpf= $_POST["cpf"];
      
-     $db.cadastros.insert($nome, $data_nascimento, $cpf) 
+     $mongo = new MongoClient("mongodb://127.0.0.1@27017");
+     $mongo -> selectDB("MOCX") -> selectCollection ("cadastros") -> insert (array ("nome" => $_POST["nome"], "data_nascimento" => $_POST["data_nascimento"], "cpf" => $_POST["cpf"]))
              
-     
-     
-     =mysqli_query($conn, "INSERT INTO lista(aluno, cpf, responsavel, endereco, fone, mail, pretendido, escola, ip, dia_visita, hora_visita, oficial)VALUES('$aluno', '$cpf', '$responsavel', '$endereco', '$fone', '$mail', '$pretendido', '$escola', '$ip, '$dia_visita', '$hora_visita', '$oficial')");
-
-        echo  "<script>alert('Gravado com Sucesso!);</script>";
-        header("Location: /Central_de_Matriculas/Best")
+      
+    
 
 ?>
